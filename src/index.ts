e07d8450e4be7ab79e2d17a2a81a85307802d736
Guilherme Moraes <guilhermejm8@gmail.com>
2022-04-10 18:17:09 -0300

    desafio finalizado

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { generate } from 'csv-generate';
 
 dotenv.config();
 
@@ -40,4 +41,35 @@ app.listen(PORT, () => {
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
 	
+	const prompt = require('prompt-sync');
+	const prt = prompt();
+	
+	let qtdeAlunos = parseInt(prt('Digite a quantidade de alunos: '));
+	
+	let listaAlunos = [];
+
+	for ( let i = 0; i < qtdeAlunos; i++) {
+		let nome = prt('Digite o nome do aluno: ');
+		let idade = parseInt(prt('Digite a idade do aluno: '));
+		let nota = parseInt(prt('Digite a nota do aluno: '));
+		let Aluno = {nome, idade, nota};
+		listaAlunos.push(Aluno);
+	}
+	let somaNotas = listaAlunos.map(Aluno => Aluno.nota).reduce((prev,curr) => prev + curr , 0);
+	const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+	const csvWriter = createCsvWriter({
+		path: './file.csv',
+		header: [
+			{id:'nome', title:'Nome'},
+			{id: 'idade', title: 'Idade'},
+			{id: 'nota', title: 'Nota'},
+			{id:'somaNotas', title:console.log('total das notas é: ' + somaNotas)}
+		]	
+	}) 
+	csvWriter.writeRecords(listaAlunos)
+		.then(() => {
+			console.log('Salvo com sucesso!');
+		});
 });
+
+ 
